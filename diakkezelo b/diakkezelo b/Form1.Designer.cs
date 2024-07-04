912d859c47079488289133010b0bf84cc37e519a
unknown <andrasgaal0209@gmail.com>
2024-07-04 11:11:05 +0200

    commit

---
+++ diakkezelo b/diakkezelo b/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace diakkezelo_b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gombok = new System.Windows.Forms.Panel();
+            this.adatbe = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(125, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Diákok";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(497, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Születési év";
+            // 
+            // gombok
+            // 
+            this.gombok.AutoScroll = true;
+            this.gombok.Location = new System.Drawing.Point(430, 78);
+            this.gombok.Name = "gombok";
+            this.gombok.Size = new System.Drawing.Size(269, 48);
+            this.gombok.TabIndex = 5;
+            // 
+            // adatbe
+            // 
+            this.adatbe.Location = new System.Drawing.Point(128, 415);
+            this.adatbe.Name = "adatbe";
+            this.adatbe.Size = new System.Drawing.Size(75, 23);
+            this.adatbe.TabIndex = 6;
+            this.adatbe.Text = "Adatbevitel";
+            this.adatbe.UseVisualStyleBackColor = true;
+            this.adatbe.Click += new System.EventHandler(this.adatbe_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 316);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(369, 208);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(390, 186);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Brown;
+            this.label3.Location = new System.Drawing.Point(364, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.adatbe);
+            this.Controls.Add(this.gombok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel gombok;
+        private System.Windows.Forms.Button adatbe;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ diakkezelo b/diakkezelo b/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Printing;
+
+namespace diakkezelo_b
+{
+    public partial class Form1 : Form
+    {
+        public List<Diak> diak = new List<Diak>();
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            openFileDialog1.FileName = "";
+        }
+
+        private void adatbe_Click(object sender, EventArgs e)
+        {
+            DialogResult eredmeny = openFileDialog1.ShowDialog();
+            if (eredmeny == DialogResult.OK)
+            {
+                string fajlNev = openFileDialog1.FileName;
+                try
+                {
+
+                    AdatBeolvasas(fajlNev);
+                    FelrakDiakok();
+                  
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Hiba a fájl beolvasásakor", "Hiba");
+                }
+            }
+
+            List<int> szamok = new List<int>();
+
+            for (int i = 0; i < diak.Count; i++)
+            {
+                if (!szamok.Contains(diak[i].ido))
+                {
+                    szamok.Add(diak[i].ido);
+                }
+            }
+            for (int i = 0; i < szamok.Count; i++)
+            {
+               Button b = new Button();
+                b.Text = Convert.ToString(szamok[i]);
+                b.Left = gombok.AutoScrollPosition.X + 60 * gombok.Controls.Count;
+                b.Width = 45;
+                b.Click += Kivalaszt;
+
+                gombok.Controls.Add(b);
+
+            }
+
+        }
+        private void AdatBeolvasas(string fajlNev)
+        {
+
+            StreamReader sr = new StreamReader(fajlNev);
+            while (!sr.EndOfStream)
+            {
+                string sor;
+                string[] s;
+                sor = sr.ReadLine();
+                s = sor.Split(';');
+                diak.Add(new Diak(s[0], s[1], int.Parse(s[2])));
+            }
+            sr.Close();
+        }
+        private void FelrakDiakok()
+        {
+            for (int i = 0; i < diak.Count; i++)
+            {
+                listBox1.Items.Add(diak[i]);
+            }
+        }
+        public void Kivalaszt(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            int kiv = int.Parse((sender as Button).Text);
+
+            for (int i = 0; i < diak.Count; i++)
+            {
+                if (diak[i].ido == kiv)
+                {
+                    listBox2.Items.Add(diak[i]);
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = $"{diak[listBox2.SelectedIndex].Nev} {diak[listBox2.SelectedIndex].kod} {diak[listBox2.SelectedIndex].ido}";
+        }
+    }
+
+
+    public class Diak
+    {
+        public string Nev;
+        public string kod;
+        public int ido;
+
+        public Diak(string nev, string kod, int ido)
+        {
+            Nev = nev;
+            this.kod = kod;
+            this.ido = ido;
+        }
+        public override string ToString()
+        {
+            return Nev + ' ' + kod;
+        }
+
+    }
+
+}
